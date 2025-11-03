4b6e6c011e68d29b361fa11301b8ad4a9e0bfec0
rubansi <rubansi24@gmail.com>
2025-11-03 09:46:47 +0300

    task susbmission

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function WelcomeMessage() {
+  return (
+    <div className="welcome-message">
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>I am learning about JSX!</p>
+    </div>
+  )
+}
+
+export default WelcomeMessage
