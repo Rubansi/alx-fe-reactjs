65134b333df9fcaf6fe3f50ad3884548f170fb6e
rubansi <rubansi24@gmail.com>
2025-11-09 21:01:00 +0300

    task submission

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  const navStyle = {
+    backgroundColor: '#333',
+    padding: '1rem',
+    marginBottom: '20px'
+  }
+
+  const linkStyle = {
+    color: 'white',
+    textDecoration: 'none',
+    marginRight: '20px'
+  }
+
+  return (
+    <nav style={navStyle}>
+      <Link to="/" style={linkStyle}>Home</Link>
+      <Link to="/about" style={linkStyle}>About</Link>
+      <Link to="/services" style={linkStyle}>Services</Link>
+      <Link to="/contact" style={linkStyle}>Contact</Link>
+    </nav>
+  )
+}
+
+export default Navbar
